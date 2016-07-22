6dde5c2adbfb523a8a5fa22c3a78e49efd5241c0
vforgione <v.forgione@gmail.com>
2016-07-21 23:16:14 -0500

    major clean up

--- osx-provision.sh
+++ osx-provision.sh
@@ -3,38 +3,33 @@
 echo "installing homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo "installing brew apps"
-brew install ack brew-cask curl git htop-osx libyaml node openssl python python3 ruby tree vim wget xz
-
-echo "installing brew cask apps"
-brew cask install iterm2 spectacle sublime-text3 vagrant vagrant-manager virtualbox
-
-echo "installing pip apps"
-pip install -U pip ansible virtualenv
+echo "installing brew-cask and git"
+brew install ack brew-cask git
 
 echo "cloning dotfiles"
-cd ~
-git clone https://github.com/vforgione/dotfiles.git Dotfiles
+git clone https://github.com/vforgione/dotfiles.git ~/Dotfiles
+
+echo "installing brew apps"
+cat ~/Dotfiles/brew.txt | xargs brew install
+
+echo "installing brew cask apps"
+cat ~/Dotfiles/brew-cask.txt | xargs brew-cask install
+
+echo "installing pip apps"
+pip install -r ~/Dotfiles/system-pip.txt
 
 echo "installing custom font"
 open ~/Dotfiles/Inconsolata\ for\ Powerline.otf
 
 echo "symlinking rc's"
-ln -s ~/Dotfiles/aliases ~/.aliases
+ln -s ~/Dotfiles/bash_profile ~/.bash_profile
 ln -s ~/Dotfiles/gemrc ~/.gemrc
 ln -s ~/Dotfiles/gitconfig ~/.gitconfig
 ln -s ~/Dotfiles/inputrc ~/.inputrc
 ln -s ~/Dotfiles/vimrc ~/.vimrc
-ln -s ~/Dotfiles/bash_profile ~/.bash_profile
 
 echo "generating new rsa key for machine"
 mkdir ~/.ssh
 cd ~/.ssh
 ssh-keygen -t rsa -b 4096
 echo "don't forget to add this to github"
-
-echo "downloading pycharm - you'll have to manually install it"
-open "https://www.jetbrains.com/pycharm/download/"
-
-echo "downloading transmission - you'll have to manually install it"
-wget "http://download.transmissionbt.com/files/Transmission-2.84.dmg"
